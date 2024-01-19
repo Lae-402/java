11cdd5bc2e8a6a4722c3b1a9f7f4b68adb239e57
Lae-402 <e5bd81.lae@gmail.com>
2024-01-19 12:24:56 +0900

    Commit

---
+++ work/Q11/CoinCase.java
@@ -0,0 +1,70 @@
+package work.Q11;
+import java.util.Random;
+
+class CoinCase {
+
+    public int I;
+    public int V;
+    public int X;
+    public int L;
+    public int C;
+    public int D;
+
+    // CoinCase(int I, int V, int X, int L, int C, int D) {
+    //     this.I = I;
+    //     this.V = V;
+    //     this.X = X;
+    //     this.L = L;
+    //     this.C = C;
+    //     this.D = D;
+    // }
+    CoinCase() {
+        this.I = 0;
+        this.V = 0;
+        this.X = 0;
+        this.L = 0;
+        this.C = 0;
+        this.D = 0;
+    }
+
+    public String getCount () {
+        return ("1円：" + I + "枚　" + "5円：" + V + "枚" + "10円：" + X + "枚　" + "50円：" + L + "枚　" + "100円：" + C + "枚　" + "500円：" + D + "枚");
+    }
+
+    public int getAmount () {
+        return (1*I + 5*V + 10*X + 50*L + 100*C + 500*D);
+    }
+
+    public void addCoins () {
+
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int coinType = random.nextInt(6);
+
+            switch (coinType) {
+                case 0:
+                    I++;
+                    break;
+                case 1:
+                    V++;
+                    break;
+                case 2:
+                    X++;
+                    break;
+                case 3:
+                    L++;
+                    break;
+                case 4:
+                    C++;
+                    break;
+                case 5:
+                    D++;
+                    break;
+            }
+        }
+
+        System.out.println(getCount());
+        System.out.println("総額：" + String.format("%,d", getAmount()) + "円");
+    }
+
+}
---
+++ work/Q11/Q11x4.java
@@ -0,0 +1,10 @@
+package work.Q11;
+
+class Q11x4 {
+
+    public static void main(String[] args) {
+
+        CoinCase a = new CoinCase();
+        a.addCoins();
+    }
+}
