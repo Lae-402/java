a8c14c4926505d35ecbaa6f0a800fb9cf205c544
Lae-402 <e5bd81.lae@gmail.com>
2024-01-19 11:26:54 +0900

    Commit

---
+++ work/Q11/BookInfo.java
@@ -0,0 +1,19 @@
+package work.Q11;
+
+public class BookInfo {
+
+    public String title;
+    public String auth;
+    public int pub_year;
+    public int book_id;
+    // static＝静的フィールド（そのクラスの全インスタンスで共有されるクラス変数）
+    private static int counter = 0;
+
+    BookInfo(String title, String auth, int pub_year) {
+        this.title = title;
+        this.auth = auth;
+        this.pub_year = pub_year;
+        this.book_id = ++counter;
+    }
+    
+}
--- work/Q11/Q11x7.java
+++ work/Q11/Q11x7.java
@@ -1,1 +1,19 @@
 package work.Q11;
+
+class Q11x7 {
+    public static void main(String[] args) {
+
+        Student[] students = {
+            new Student("A", "001", 89, 65, 88),
+            new Student("B", "002", 80, 95, 64),
+            new Student("C", "003", 70, 80, 98)
+        };
+
+        for (Student student : students) {
+            double ave = student.calc_ave();
+            ave = (int)(ave * 100);
+            ave = ave / 100;
+            System.out.println(student.id + " 番　" + student.name + "さん　平均点" + ave);
+        }
+    }
+}
